d00d7bba5ceba4ead316a58dc5c20253cb91bfd5
Fatime31 <fatimendiaye14@gmail.com>
2023-03-31 14:04:57 +0200

    changement valeur spinner + setonclicklistener

--- app/src/main/java/com/example/miniprojet/AddActivity.java
+++ app/src/main/java/com/example/miniprojet/AddActivity.java
@@ -1,7 +1,9 @@
 package com.example.miniprojet;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,11 +26,19 @@ public class AddActivity extends AppCompatActivity {
 
         Spinner spin = (Spinner) findViewById(R.id.spin_matiere);
         String [] valeurs = getResources().getStringArray(R.array.valeurs);
+        Button btn_annuler = (Button) findViewById(R.id.btn_annuler);
 
         this.listeValeurs = new ArrayList<>();
         Collections.addAll(this.listeValeurs,valeurs);
 
         this.adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,this.listeValeurs);
         spin.setAdapter(adapter);
+
+        btn_annuler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
